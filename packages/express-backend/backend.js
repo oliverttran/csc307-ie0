ee3b27db2e1b7b83c9f6206f2317e7c432f28e36
oliverttran <olivertran114@gmail.com>
2026-01-21 23:20:54 -0800

    added deletebyid

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -85,3 +85,24 @@ app.post("/users", (req, res) => {
   addUser(userToAdd);
   res.send();
 });
+
+const deleteUserById = (id) => {
+    const index = users["users_list"].findIndex((user) => user["id"] === id);
+
+    if (index === -1) {
+        return null;
+    }
+
+    return users["users_list"].splice(index, 1)[0];
+};
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deletedUser = deleteUserById(id);
+
+  if (!deletedUser) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send();
+  }
+});
